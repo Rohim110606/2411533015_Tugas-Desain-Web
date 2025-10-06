4091124c624c553eba8475a16c1206d7454ea629
Rohim110606 <abdurrohim2715@gmail.com>
2025-10-06 19:20:49 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "pwa-cache-v1";
+const CACHE_NAME = "pwa-cache-v2";
 const urlsToCache = [
-  ".",
+  "./",
   "index.html",
   "about.html",
   "kegiatan.html",
   "contact.html",
-  "offline.html",   // halaman offline
-  "donatcandy.png",
+  "offline.html",
+  "donatcandy.png",            
   "gambar%202/pemandangan.jpg"
 ];
 
@@ -17,14 +17,33 @@ self.addEventListener("install", (event) => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
-// Fetch Event
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch: Offline Fallback
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches.match(event.request).then((response) => {
-        return response || caches.match("/offline.html"); // fallback offline
+        if (response) {
+          return response;
+        } else if (event.request.mode === "navigate") {
+          return caches.match("offline.html");
+        }
       });
     })
   );
